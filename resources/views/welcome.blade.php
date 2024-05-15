9aaa3946ee51cca7a16b5283e11152142457f499
maaeyeng <yarangit@yahoo.com>
2024-05-15 11:30:27 +0700

    new

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ERP: Enterprise Resource Planning System</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container">
+    <ul class="nav p-2">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Dashboard</a>
+        </li>
+        <li class="nav-item text-Light">
+          <a class="nav-link text-Light " href="#">Import</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-Light  " href="#">Tracking</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-Light  ">DMS</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-Light  ">Report</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-Light  ">CCR</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-Light  ">Mall</a>
+          </li>
+      </ul>
+</div>
+
+<div class="container" style="width:15%;float:left;">
+    <ul class="nav flex-column">
+        <li class="nav-item text-light">
+          <a class="nav-link active" aria-current="page" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+</div>
+<div class="container pl-5 bg-primary" style="width:60%;float;left;">
+    <h1>Content body </h1>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
